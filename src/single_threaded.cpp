2cc1e5fbbea7988583ca533a104225fab3029cf0
Mark Blanco <mrkblnc1248@gmail.com>
2017-03-29 20:14:15 -0400

    Add main file for naive single threaded truss solver.

---
+++ src/single_threaded.cpp
@@ -0,0 +1,9 @@
+// Date: 29 March 2017
+// Description: Single threaded naive implementation of truss solver
+// Based on: https://www.cs.princeton.edu/courses/archive/fall09/cos323/assign/truss/truss.html
+
+int main( int argc, char ** argv )
+{
+	
+	return 0;
+}
