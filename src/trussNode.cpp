048a8a58bb9048fb9b01203b9ec0a8d8c460476a
Mark Blanco <mrkblnc1248@gmail.com>
2017-04-12 22:26:49 -0400

    Resolve compiler errors for trussNode.

--- src/trussNode.cpp
+++ src/trussNode.cpp
@@ -1,19 +1,60 @@
-// trussNode class
+#include "trussNode.hpp"
 
-class trussNode
+// trussNode class implementation 
+
+trussNode::trussNode(const std::string & label, double x, double y, double z, 
+	bool moveX, bool moveY, bool moveZ, double Fx, double Fy, double Fz)
+: _label(label), _coordX(x), _coordY(y), _coordZ(z), _moveX(moveX), 
+_moveY(moveY), _moveZ(moveZ), _extForceX(Fx), _extForceY(Fy), _extForceZ(Fz)
 {
-public:
-	trussNode(const &std::string label, double x, double y, double z, 
-		bool moveX, bool moveY, bool moveZ)
-	: _label(label), _coordX(x), _coordY(y), _coordZ(z), _moveX(moveX), _moveY(moveY), _moveZ(moveZ)
+	_neighbors = std::vector<trussNode>();
+	_sections = std::vector<double>();
+	_E = std::vector<double>();
+}
+void trussNode::addNeighbor(const trussNode & adj, double section, double E)
+{
+	_neighbors.push_back(adj);
+	_sections.push_back(section);
+	_E.push_back(E);
+}
+
+double const trussNode::distTo( trussNode const & adj)
+{
+	double dx = _coordX - adj.getXCoord();
+	double dy = _coordY - adj.getYCoord();
+	double dz = _coordZ - adj.getZCoord();
+	return sqrt( dx*dx + dy*dy * dz*dz );
+}
+
+double const trussNode::xProjNormTo( trussNode const & adj)
+{
+	double mag = this->distTo(adj);
+	double dx = abs( _coordX - adj.getXCoord() );
+	if ( _coordX > adj.getXCoord() )
 	{
-		_neighbors = std::vector();
-		_sections = std::vector();
-		_E = std::vector();
+		return dx/mag;
 	}
-	void addNeighbor(const &trussNode adj)
+	return -1 * (dx/mag);
+}
+
+double const trussNode::yProjNormTo( trussNode const & adj)
+{
+	double mag = this->distTo(adj);
+	double dy = abs( _coordY - adj.getYCoord() );
+	if ( _coordY > adj.getYCoord() )
 	{
-		_neighbors.push_back(adj)
+		return dy/mag;
 	}
-	
-};
+	return -1 * (dy/mag);
+}
+
+double const trussNode::zProjNormTo( trussNode const & adj)
+{
+	double mag = this->distTo(adj);
+	double dz = abs( _coordY - adj.getYCoord() );
+	if ( _coordY > adj.getYCoord() )
+	{
+		return dz/mag;
+	}
+	return -1 * (dz/mag);
+}
--- src/trussNode.hpp
+++ src/trussNode.hpp
@@ -1,17 +1,27 @@
 #include <vector>
 #include <string>
-#include <stdio>
+#include <cstdio>
+#include <cmath>
+#include <vector>
+#include <string>
 
 class trussNode
 {
 public:
-	trussNode(const &std::string label, double x, double y, double z, 
-		bool moveX, bool moveY, bool moveZ);
-	void addNeighbor(const &trussNode adj);
-	void getPosition();
-
+	trussNode( std::string const & label, double x, double y, double z, 
+		bool moveX, bool moveY, bool moveZ, double Fx, double Fy, double Fz);
+	void addNeighbor( trussNode const & adj, double section, double E);
+	double const getXCoord() const { return this->_coordX; }
+	double const getYCoord() const { return this->_coordY; }
+	double const getZCoord() const { return this->_coordZ; }
 
 private:
+// Helper functions:
+	double const distTo( trussNode const & adj);
+	double const xProjNormTo( trussNode const & adj);
+	double const yProjNormTo( trussNode const & adj);
+	double const zProjNormTo( trussNode const & adj);
+
 // Representation:
 	std::string _label;
 	bool _moveX;
@@ -20,10 +30,13 @@ private:
 	double _coordX;
 	double _coordY;
 	double _coordZ;
+	double _extForceX;
+	double _extForceY;
+	double _extForceZ;
 	// Vector of neighboring (connected) vertices
 	std::vector<trussNode> _neighbors;
 	// Vector of member cross sectional areas corresponding to each neighbor
 	std::vector<double> _sections;
 	// Vector of Young's Moduli for each member
 	std::vector<double> _E;
-}
+};
