77a596f06573f62393c9d132c166793667c8eaf8
Nathan Bernard <natedawg1013@gmail.com>
2017-05-01 16:05:03 -0400

    Added color rendering, more interesting example truss

--- src/webUI/render.js
+++ src/webUI/render.js
@@ -6,16 +6,20 @@ var canvas;
 var submit, url, submit2, file;
 
 var camera = {
-    near: 0.1,
-    far: 10,
+    near: 0.01,
+    far: 100,
     aspect: 0.7,
-    fovAngle: 45,
+    fovAngle: 90,
     z: 4.0
 }
 
 var dx=0,dy=0,dz=0;
 
+var data;
+
 var modelViewMatrixLoc, projectionMatrixLoc;
+
+var nodeprogram, edgeprogram;
 
 var radius = 6.0;
 
@@ -40,12 +44,16 @@ window.onload = function init()
     
     submit=document.getElementById( "submit" );
     url = document.getElementById( "url" );
+    /*
     submit2=document.getElementById( "submit2" );
     file = document.getElementById( "file" );
+    */
     submit.onclick=getAndDrawUrl;
+    /*
     submit2.onclick=function(){
         getAndDrawFile(this.files[0]);
     }
+    */
 
     var shift = false;
 
@@ -97,7 +105,6 @@ window.onload = function init()
             theta=lastTheta-xDiff;
             phi=lastPhi+yDiff;
         }
-        console.log(e.shiftKey);
         render(); // render to show changes
 
     }
@@ -108,10 +115,12 @@ window.onload = function init()
     {
         event.preventDefault();
         if (event.deltaY > 0) camera.z += 0.1;
-        else camera.z -= 0.1;      
+        else camera.z -= 0.1;
+        if(camera.z<0.1) camera.z=0.1;  
         render();  
     })
 
+    getAndDrawUrl();
 
 };
 
@@ -148,7 +157,7 @@ function getAndDrawUrl(){
 
 function drawTruss(raw){
 
-    var data = JSON.parse(raw);
+    data = JSON.parse(raw);
 
     vertices = [];
     edges = []; 
@@ -161,37 +170,10 @@ function drawTruss(raw){
         edges.push(data.Edges[i].Endpoints);
     }
 
-    vertices = normalizeShape(vertices);
+    camera.z=zoomToFit(vertices);
 
-    //
-    //  Configure WebGL
-    //
     gl.viewport( 0, 0, canvas.width, canvas.height );
     gl.clearColor( 0.0, 0.0, 0.0, 1.0 );
-    
-    //  Load shaders and initialize attribute buffers
-    
-    var program = initShaders( gl, "vertex-shader", "fragment-shader" );
-    gl.useProgram( program );
-
-    modelViewMatrixLoc = gl.getUniformLocation( program, "modelViewMatrix" );
-    projectionMatrixLoc = gl.getUniformLocation( program, "projectionMatrix" );
-
-        // Load the data into the GPU
-    
-    bufferId = gl.createBuffer();
-    gl.bindBuffer( gl.ARRAY_BUFFER, bufferId );
-    gl.bufferData( gl.ARRAY_BUFFER, flatten(vertices), gl.STATIC_DRAW );
-
-    edgeIndexBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, edgeIndexBuffer);
-    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(flatten(edges)), gl.STATIC_DRAW);
-
-    // Associate out shader variables with our data buffer
-    
-    var vPosition = gl.getAttribLocation( program, "vPosition" );
-    gl.vertexAttribPointer( vPosition, 3, gl.FLOAT, false, 0, 0 );
-    gl.enableVertexAttribArray( vPosition );
 
     render();
 }
@@ -206,23 +188,81 @@ function render(){
 
     if(Math.cos(phi)<0) {
         up[1]=-1;
-        console.log("inverse up");
     }
 
     modelViewMatrix = lookAt( eye, at, up );
     projectionMatrix = perspective(camera.fovAngle, camera.aspect, camera.near, camera.far);
 
-    gl.uniformMatrix4fv( modelViewMatrixLoc, false, flatten(modelViewMatrix) );
-    gl.uniformMatrix4fv( projectionMatrixLoc, false, flatten(projectionMatrix) );
-
+    gl.clear( gl.COLOR_BUFFER_BIT );
+    renderEdges();
+    renderNodes();
     
 
-    gl.clear( gl.COLOR_BUFFER_BIT );
+}
+
+function renderNodes(){
+    nodeprogram = initShaders( gl, "node-vertex-shader", "node-fragment-shader" );
+    gl.useProgram( nodeprogram );
+
+    modelViewMatrixLoc = gl.getUniformLocation( nodeprogram, "modelViewMatrix" );
+    projectionMatrixLoc = gl.getUniformLocation( nodeprogram, "projectionMatrix" );
+
+    gl.uniformMatrix4fv( modelViewMatrixLoc, false, flatten(modelViewMatrix) );
+    gl.uniformMatrix4fv( projectionMatrixLoc, false, flatten(projectionMatrix) );
+        // Load the data into the GPU
+    
+    bufferId = gl.createBuffer();
+    gl.bindBuffer( gl.ARRAY_BUFFER, bufferId );
+    gl.bufferData( gl.ARRAY_BUFFER, flatten(vertices), gl.STATIC_DRAW );
+
+    var vPosition = gl.getAttribLocation( nodeprogram, "vPosition" );
+    gl.vertexAttribPointer( vPosition, 3, gl.FLOAT, false, 0, 0 );
+    gl.enableVertexAttribArray( vPosition );
 
     gl.bindBuffer( gl.ARRAY_BUFFER, bufferId );
     gl.drawArrays( gl.POINTS, 0, vertices.length);
-    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, edgeIndexBuffer);
-    gl.drawElements(gl.LINES, edges.length*2, gl.UNSIGNED_SHORT, 0)
+
+}
+
+function renderEdges(){
+    edgeprogram = initShaders( gl, "edge-vertex-shader", "edge-fragment-shader" );
+    gl.useProgram( edgeprogram );
+
+    modelViewMatrixLoc = gl.getUniformLocation( edgeprogram, "modelViewMatrix" );
+    projectionMatrixLoc = gl.getUniformLocation( edgeprogram, "projectionMatrix" );
+    
+    gl.uniformMatrix4fv( modelViewMatrixLoc, false, flatten(modelViewMatrix) );
+    gl.uniformMatrix4fv( projectionMatrixLoc, false, flatten(projectionMatrix) );
+
+    var points = [];
+
+    var ver = [];
+
+    for(var i=0;i<data.Vertices.length;i++){
+        ver.push(vec3(data.Vertices[i].XYZPosition));
+    }
+
+    for(var i=0;i<data.Edges.length;i++){
+        points.push(vec4(ver[data.Edges[i].Endpoints[0]],data.Edges[i].Force));
+        points.push(vec4(ver[data.Edges[i].Endpoints[1]],data.Edges[i].Force));
+        
+    }
+    console.log(flatten(points));
+
+    var edgebufferId = gl.createBuffer();
+    gl.bindBuffer( gl.ARRAY_BUFFER, edgebufferId );
+    gl.bufferData( gl.ARRAY_BUFFER, flatten(points), gl.STATIC_DRAW );
+    
+    var vPosition = gl.getAttribLocation( edgeprogram, "vPosition" );
+    gl.vertexAttribPointer( vPosition, 3, gl.FLOAT, false, 16, 0 );
+    var hue = gl.getAttribLocation( edgeprogram, "hue" );
+    gl.vertexAttribPointer( hue, 1, gl.FLOAT, false, 16, 12 );
+    gl.enableVertexAttribArray( vPosition );
+    gl.enableVertexAttribArray( hue );
+
+    gl.bindBuffer( gl.ARRAY_BUFFER, edgebufferId );
+    gl.drawArrays( gl.LINES, 0, data.Edges.length*2);
+
 }
 
 function normalizeShape(data){
@@ -254,4 +294,23 @@ function normalizeShape(data){
         out.push([x, y, z])
     }
     return out;
+}
+
+function zoomToFit(data){
+    var mag=0.00;
+    var maxX, minX, maxY, minY, maxZ, minZ;
+    maxX = minX = maxY = minY = maxZ = minZ = 0;
+    for(i=0; i<data.length; i++){
+        if(data[i][0]>maxX) maxX=data[i][0];
+        if(data[i][0]<minX) minX=data[i][0];
+        if(data[i][1]>maxY) maxY=data[i][1];
+        if(data[i][1]<minY) minY=data[i][1];
+        if(data[i][2]>maxZ) maxZ=data[i][2];
+        if(data[i][2]<minZ) minZ=data[i][2];
+    }
+    mag=maxX-minX;
+    if(mag < maxY-minY) mag = maxY-minY;
+    if(mag < maxZ-minZ) mag = maxZ-minZ;
+    mag=mag;
+    return mag;
 }
