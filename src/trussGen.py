5f89e6933afe7c55ba86c11e1240b22d791cf19a
Mark Blanco <mrkblnc1248@gmail.com>
2017-05-03 13:34:15 -0400

    Add truss file generator written in Python.

---
+++ src/trussGen.py
@@ -0,0 +1,36 @@
+# Generate 3D truss files in json to be viewed and solved
+
+import sys
+import json
+
+if len(sys.argv) < 3:
+	print("USAGE: {} truss_repetitions output_filename.json".format(sys.argv[0]))
+# Number of adjacent truss sections that will be created and connected
+num_repetitions = int(sys.argv[1])
+
+SectionArea = 0.25
+ElasticModulus = 2500
+
+# Base 2d template truss structure, which can be repeated and connected adjacently
+locations = [ [0.0, 0.0, 0.0], [3.0, 2.0, 0.0], [6.0, 4.0, 0.0], [9.0, 2.0, 0.0], [12.0, 0.0, 0.0], [8.0, 0.0, 0.0], [4.0, 0.0, 0.0] ]
+connections = [ [0, 1], [1, 2], [2, 3], [3, 4], [4, 5], [5, 6] ]
+#inter_layer_connections [  ]
+json_to_dump = { 'Vertices':[], 'Edges':[] }
+
+for i in range(num_repetitions):
+	for loc in locations:
+		loc[2] += i
+		vertex = { 'XYZPosition':loc, 'XYZAppliedForces':[0.0,0.0,0.0], 'Anchored':[0.0,0.0,0.0] }
+		json_to_dump['Vertices'].append(vertex)
+
+	for conn in connections:
+		offset = len(locations) * i
+		conn = [x+offset for x in conn]
+		edge = { 'Endpoints':conn, 'ElasticModulus':ElasticModulus, 'SectionArea':SectionArea, 'Force':0.0, 'Stress':0.0 }
+		json_to_dump['Edges'].append(edge)
+
+with open(sys.argv[2], 'w') as f:
+	json.dump(json_to_dump, f)
+	f.close()
+
+# Tesselate 2D triangles?:
