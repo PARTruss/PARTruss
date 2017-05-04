9b8fc61f5e657bc312b677ee7000a213147807ff
Nathan Bernard <natedawg1013@gmail.com>
2017-05-04 15:33:32 -0400

    limited max matrix print dimension to 20

--- src/util.hpp
+++ src/util.hpp
@@ -2,14 +2,16 @@
 
 #include <iostream>
 #include <iomanip>
+#define MAXMTXPRINT 20
 
 using namespace std;
 
 template <typename t> 
 void printMtx(t* A, int n, int width){
-	for(int i=0;i<n;i++){
-		for(int j=0;j<n;j++){
-      cout<<setfill(' ')<<defaultfloat<<setw(width)<<setprecision(1)<<A[n*i+j];
+	int max = (n<MAXMTXPRINT) ? n : MAXMTXPRINT;
+	for(int i=0;i<max;i++){
+		for(int j=0;j<max;j++){
+      		cout<<setfill(' ')<<defaultfloat<<setw(width)<<setprecision(1)<<A[n*i+j];
 		}
 		cout<<endl;
 	}
