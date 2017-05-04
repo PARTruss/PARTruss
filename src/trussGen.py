bfcf8fb69e618051a64213a2604c4d25f280ef12
Drake Perrior-small <perrid3@rpi.edu>
2017-05-03 21:46:21 -0400

    tenative centering

--- src/trussGen.py
+++ src/trussGen.py
@@ -14,11 +14,16 @@ ElasticModulus = 25000000000
 
 # Base 2d template truss structure, which can be repeated and connected adjacently
 locations = [ [0.0, 0.0, 0.0], [3.0, 2.0, 0.0], [6.0, 4.0, 0.0], [9.0, 2.0, 0.0], [12.0, 0.0, 0.0], [8.0, 0.0, 0.0], [4.0, 0.0, 0.0] ]
+delta = [sum([ x[0] for x in locations])/len(locations), \
+	 sum([ x[1] for x in locations])/len(locations), \
+	 sum([ x[0] for x in locations])/len(locations) ]
+locations = [[i[0]-delta[0],i[1]-delta[1],i[2]-delta[2]] for i in locations]
+print locations
 connections = [ [0, 1], [1, 2], [2, 3], [3, 4], [4, 5], [5, 6], [6, 0], [1, 6], [2, 6], [2, 5], [3, 5] ]
 #inter_layer_connections [ [], [], [], [] ]
 json_to_dump = { 'Vertices':[], 'Edges':[] }
 
-for i in range(num_repetitions):
+for i in range(-1 * (num_repetitions/2),num_repetitions/2 + 1):
 	for loc in locations:
                 # Increment the Z value
 		loc[2] += i
